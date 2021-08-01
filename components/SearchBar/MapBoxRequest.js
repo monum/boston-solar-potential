63c4142fc62e57b19b79b5b88061d1a81e2ec64e
nojibe <chibizoar@gmail.com>
2021-08-01 19:13:18 -0400

    autocomplete adjustment

---
+++ components/SearchBar/MapBoxRequest.js
@@ -0,0 +1,21 @@
+import React, {useEffect, Component } from 'react';
+import ReactMap
+//components
+
+const token = "pk.eyJ1Ijoibm9qaWJlIiwiYSI6ImNrcHloOXg1OTA0M3cyb21uYW83d2V3MGwifQ.GlZLAGPrDIf1lihGKJIBqw"
+
+
+export default function MapReq(streetname){
+    
+    const handleClickStreet = async searchLocation() {
+
+    }
+
+
+
+
+
+    return(
+
+    );
+}
